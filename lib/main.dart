d8338fe7fd22bce05427053e87ab7c5d5911834b
pliniogim <pliniogimenez@gmail.com>
2022-05-01 19:28:17 -0300

    20220501

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,78 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: const Text('Dicee'),
+          title: const Text('Dices'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var random = Random();
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const [
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+
+    void changeDice() {
+      setState(() {
+        leftDiceNumber = random.nextInt(6) + 1;
+        rightDiceNumber = random.nextInt(6) + 1;
+      });
+    }
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                style: TextButton.styleFrom(primary: Colors.transparent),
+                onPressed: () {
+                  changeDice();
+                },
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                style: TextButton.styleFrom(primary: Colors.transparent),
+                onPressed: () {
+                  changeDice();
+                },
+                child: Image.asset(
+                  'images/dice$rightDiceNumber.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
